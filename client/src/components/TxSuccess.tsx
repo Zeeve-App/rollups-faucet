58d27ccf1b6e9bd2db3d40b4882635a08255b18c
prakash.h <prakash.h@zeeve.io>
2025-07-15 19:27:20 +0530

    fix: UI Fixes

--- client/src/components/TxSuccess.tsx
+++ client/src/components/TxSuccess.tsx
@@ -11,7 +11,7 @@ const TxSuccess = ({ msg, txHash, explorerUrl, goBack }: TxSuccessProps) => {
   return (
     <div className="tx-success">
       <div className="success-msg">
-        Transaction successful on Parfin Testnet!{" "}
+        Transaction successful on Rayls Bullet Testnet!{" "}
       </div>
       <div className="">
         <div className="field" style={{ marginBottom: "12px" }}>
